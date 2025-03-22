ed8edf8d0a408b647078e01aba63ba2e35bcfbdf
Amal7654433 <121153174+Amal7654433@users.noreply.github.com>
2025-03-22 23:10:45 +0530

    pages and routes created

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+import { connectDb } from './configs/dbConnection.js'
+// import userRouter from './routes/user.js'
+// import adminRouter from './routes/admin.js'
+import cors from 'cors'
+import logger from "morgan";
+import cookieParser from 'cookie-parser'
+
+// import errorHandler from './middlewares/errorHandler.js'
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+const app = express()
+dotenv.config()
+app.use(logger('dev'));
+app.use(express.json());
+
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+
+}));
+
+// app.use(adminRouter)
+// app.use(userRouter)
+// app.use(errorHandler);
+app.listen(4000, () => {
+    connectDb()
+    console.log('server started')
+})
